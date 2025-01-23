550c6a6904468989bd2536757608aab735998f07
Parthip.P.R <105987430+ParthipPR@users.noreply.github.com>
2025-01-23 10:35:52 +0530

    Revert "Feat: added Chat room features"

--- forum/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumer
-
-websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumer.ChatConsumer.as_asgi()),  # Ensure this pattern is correct
-]
--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('question_list', views.question_list, name='question_list'),
     path('question/<int:question_id>/', views.question_detail, name='question_detail'),
@@ -9,9 +8,5 @@ urlpatterns = [
     path('question/<int:question_id>/upvote/<int:answer_id>/', views.upvote_answer, name='upvote'),
     path('delete_answer/<int:answer_id>/', views.delete_answer, name='delete_answer'),
     path('question/<int:question_id>/delete/', views.delete_question, name='delete_question'),
-    path('chat-rooms/', views.chat_room_list, name='chat_room_list'),
-    path('create-chat-room/', views.create_chat_room, name='create_chat_room'),
-    path('chat-room/<int:room_id>/delete/', views.delete_chat_room, name='delete_chat_room'),
-    path('chat/<str:room_name>/', views.chat_room, name='chat_room'),
 ]
 
